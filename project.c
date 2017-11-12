b23ca4e81a97f468e7829489b286578c813978ca
Jonatas <maximianojonatas@gmail.com>
2017-11-12 05:57:23 -0200

    Add Error treatment to wrong answers

--- project.c
+++ project.c
@@ -35,6 +35,9 @@ int status = 0;
 int torreAtual = 0;
 int tempoEncerrado = 0;
 int recomecar = 1;
+int respostaErrada,respostaCorreta; 
+int respostasCorretas[9] = {-3,9,5,2,8,6,3,7,0};
+int start = 0;
 
 
 void iluminacao(void){
@@ -1420,17 +1423,33 @@ void fimDeJogoD() {
     glutSwapBuffers();
 
 }
-
+void renderBitmapString(float x, float y, void *font,const char *string){
+    const char *c;
+    glRasterPos2f(x, y);
+    for (c=string; *c != '\0'; c++) {
+        glutBitmapCharacter(font, *c);
+    }
+} 
 void gameOver() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     //Estrutura
     eixos();
     pista();
+    //Textos 
+    if(start == 1){
+	    char buffer[10]={'\0'};
+	    sprintf(buffer, "%d", respostaErrada); 
+	    drawBitmapText("Respota Errada;", 1.5, -0.35, 0.0);
+	    drawBitmapText("Voce digitou a resposta:;", 1.45, -0.35, 0.0);
+	    renderBitmapString(1.4f, -0.35f, GLUT_BITMAP_HELVETICA_18, buffer);
+	    char buffer2[10] = {'\0'};
+	    sprintf(buffer2, "%d", respostaCorreta); 
+	    drawBitmapText("A resposta correta seria:;", 1.35, -0.35, 0.0);
+	    renderBitmapString(1.3f, -0.35f, GLUT_BITMAP_HELVETICA_18, buffer2);
+	    drawBitmapText("VOCE PERDEU!!!!!!!;", 1.25, -0.35, 0.0);
 
-    //Textos
-    drawBitmapText("Respota Errada;", 1.5, -0.35, 0.0);
-    drawBitmapText("VOCE PERDEU!!!!!!!;", 1.45, -0.35, 0.0);
+    }
     recomecar = 1;
 
     //Torres (Ordem contrária para fazer sobreposição)
@@ -1504,6 +1523,7 @@ void reshape(int w, int h) {
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
     case 'd': //Seleciona lado direito
+	start = 1;
         if(recomecar == 1){
             posx = 2.0f;
             posy = -1.0f;
@@ -1523,6 +1543,7 @@ void keyboard(unsigned char key, int x, int y) {
         }
         break;
     case 'n': //Seleciona lado esquerdo
+	start = 1;
         if(recomecar == 1){
             posx = 2.0f;
             posy = 1.0f;
@@ -1548,7 +1569,10 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreEsquerdaATimer, 10);
                 glutDisplayFunc(onTorreEsquerdaA);
             } else {
+		respostaErrada = 9;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
+
             }
         }
         break;
@@ -1558,6 +1582,8 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreEsquerdaBTimer, 10);
                 glutDisplayFunc(onTorreEsquerdaB);
             } else {
+		respostaErrada = 5;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1568,6 +1594,8 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreEsquerdaTimer, 10);
                 glutDisplayFunc(onTorreEsquerda);
             } else {
+		respostaErrada = 2;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1577,6 +1605,8 @@ void keyboard(unsigned char key, int x, int y) {
             if (torreAtual == 4) {
                 glutDisplayFunc(fimDeJogo);
             } else {
+		respostaErrada = 8;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1587,6 +1617,8 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreDireitaATimer, 10);
                 glutDisplayFunc(onTorreDireitaA);
             } else {
+		respostaErrada = 6;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1598,6 +1630,8 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreDireitaBTimer, 10);
                 glutDisplayFunc(onTorreDireitaB);
             } else {
+		respostaErrada = 3;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1608,6 +1642,8 @@ void keyboard(unsigned char key, int x, int y) {
                 glutTimerFunc(GAP, onTorreDireitaTimer, 10);
                 glutDisplayFunc(onTorreDireita);
             } else {
+		respostaErrada = 7;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1617,6 +1653,8 @@ void keyboard(unsigned char key, int x, int y) {
             if (torreAtual == 8) {
                 glutDisplayFunc(fimDeJogoD);
             } else {
+		respostaErrada = 0;
+		respostaCorreta = respostasCorretas[torreAtual];
                 glutDisplayFunc(gameOver);
             }
         }
@@ -1624,6 +1662,12 @@ void keyboard(unsigned char key, int x, int y) {
 
     default:
         if(tempoEncerrado == 0){
+	    if(key == '4'){
+	    	respostaErrada = 4;
+	    } else if (key == '1'){
+	    	respostaErrada = 1;
+	    }
+	    respostaCorreta = respostasCorretas[torreAtual];
             glutDisplayFunc(gameOver);
         }
         break;
